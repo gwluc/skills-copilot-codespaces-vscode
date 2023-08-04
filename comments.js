e3a5dbb5df597abb2bd8b4e956cbc560f70c642c
Luc Maillet <84546682+gwluc@users.noreply.github.com>
2023-08-04 17:48:03 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,38 +1,21 @@
 // create web server
 
-// import express
-const express = require("express");
-// import comment router
-const router = express.Router();
-// import comment controller
-const commentController = require("../controllers/commentController");
-// import user controller
-const userController = require("../controllers/userController");
-
-// create comment routes
-// GET request for creating comment
-router.get("/create", commentController.comment_create_get);
-
-// POST request for creating comment
-router.post("/create", commentController.comment_create_post);
-
-// GET request for deleting comment
-router.get("/:id/delete", commentController.comment_delete_get);
-
-// POST request for deleting comment
-router.post("/:id/delete", commentController.comment_delete_post);
-
-// GET request for updating comment
-router.get("/:id/update", commentController.comment_update_get);
-
-// POST request for updating comment
-router.post("/:id/update", commentController.comment_update_post);
-
-// GET request for one comment
-router.get("/:id", commentController.comment_detail);
-
-// GET request for list of all comments
-router.get("/", commentController.comment_list);
-
-// export router
-module.exports = router;
+const path = require("path");
+// import fs
+const fs = require("fs");
+// import moment
+const moment = require("moment");
+// import mongoose
+const mongoose = require("mongoose");
+// import comment model
+const Comment = require("../models/comment");
+// import user model
+const User = require("../models/user");
+// import post model
+const Post = require("../models/post");
+// import express-validator
+const { body, validationResult } = require("express-validator");
+// import sanitize
+const { sanitizeBody } = require("express-validator");
+// import async
+const async = require("async");
